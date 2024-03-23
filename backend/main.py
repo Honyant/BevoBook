ac8b0eb8e44972704dba898925377624a3086a0a
Anthony Wang <anthonyindeepspace@gmail.com>
2024-03-23 09:44:33 +0000

    backend integration

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,14 @@ from pydantic import BaseModel
 import random
 import string
 import uvicorn
+from cloudflare_api import run
+
+
+import requests
+import pandas as pd
+from helper import *
+from fuzzywuzzy import fuzz
+from config import account_id, api_token
 
 app = FastAPI()
 
@@ -21,12 +29,12 @@ app.add_middleware(
 class ChatbotInput(BaseModel):
     text: str
 
+
 @app.post("/chatbot")
 async def chatbot(input_data: ChatbotInput):
     text = input_data.text
-    # Generate a few random characters
-    random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-    return {"response": random_chars}
+    response = run(text)
+    return {"response": response}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
